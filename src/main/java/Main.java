8da45cc86bfe5c7cf8d76d744093f3a31c9fbdba
Yaroslav Ilin <C:\Users\yaros\AppData\Roaming\The Bat!>
2024-04-16 18:22:24 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,27 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите название текстового файла:");
+        System.out.println("Enter a text file name:");
         String fileName = scanner.nextLine();
 
-        System.out.println("Введите текст:");
+        System.out.println("Enter text:");
         String textInput = scanner.nextLine();
 
         try (PrintWriter out = new PrintWriter(fileName)) {
             out.println(textInput);
-            System.out.println("Текст успешно сохранен");
+            System.out.println("Text is save");
         } catch (FileNotFoundException e) {
-            System.err.println("Файл не найден: " + e.getMessage());
+            System.err.println("File not found: " + e.getMessage());
         }
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            System.out.println("Содержимое файла" + fileName + ":");
+            System.out.println("File contents" + fileName + ":");
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException e) {
-            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+            System.err.println("Error reading file: " + e.getMessage());
         }
     }
 }
